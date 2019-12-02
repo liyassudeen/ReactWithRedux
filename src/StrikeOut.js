ef2d77e2ea942bba76b75e48c5966f4c711615f8
liyassudeen <33513683+liyassudeen@users.noreply.github.com>
2019-12-02 17:58:47 +0530

    Styling
    Styling added

--- src/StrikeOut.js
+++ src/StrikeOut.js
@@ -14,21 +14,36 @@ class StrikeOut extends React.Component {
     this.textInput = React.createRef();
   }
 
+  handleSubmit() {
+    this.props.addText(this.textInput);
+  }
+
   render() {
     const list = this.props.listElement.map(item => (
       <li onClick={e => this.props.strikeOutText(e)}>{item}</li>
     ));
     return (
       <>
-        <input type="text" ref={this.textInput} />
-        <input
-          type="button"
-          value="AddText"
-          onClick={() => this.props.addText(this.textInput)}
-        />
+        <h1>Welcome to react with redux</h1>
+        <div className="form-wrapper">
+          <input
+            type="text"
+            placeholder="Type text to add list..."
+            ref={this.textInput}
+          />
+          <input
+            type="button"
+            value="AddText"
+            onClick={() => this.handleSubmit}
+          />
+        </div>
         <ul>{list}</ul>
-        <div>
-          {this.props.nonStrikeOutCount} out of {this.props.listElement.length}
+        <div className="result-group">
+          <span className="non-strike-list">
+            {this.props.nonStrikeOutCount}
+          </span>{" "}
+          out of
+          <span className="total-list"> {this.props.listElement.length}</span>
         </div>
       </>
     );
