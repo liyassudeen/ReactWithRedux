2501ea580f9327711b571adaabdcc116ea0182f3
liyassudeen <33513683+liyassudeen@users.noreply.github.com>
2019-12-03 14:32:05 +0530

    Code cleanup
    Code cleanup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import TodoList from "./task/todoList/todoList";
-//import { store } from "./task/todoList/store";
+//Task - todoList
+/* import TodoList from "./task/todoList/todoList";
+import { store } from "./task/todoList/store"; */
 
+//Task - googleAuth
 import GoogleAuth from "./task/googleAuth/googleAuth";
 import { store } from "./task/googleAuth/store";
 
