573d3028f30143304861f499a78002592797da83
liyassudeen <33513683+liyassudeen@users.noreply.github.com>
2019-12-05 11:48:31 +0530

    change root to todoList
    change root to todoList

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 //Task - todoList
-/* import TodoList from "./task/todoList/todoList";
-import { store } from "./task/todoList/store"; */
+import TodoList from "./task/todoList/todoList";
+import { store } from "./task/todoList/store";
 
 //Task - googleAuth
-import GoogleAuth from "./task/googleAuth/googleAuth";
-import { store } from "./task/googleAuth/store";
+/*import GoogleAuth from "./task/googleAuth/googleAuth";
+import { store } from "./task/googleAuth/store";*/
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={store}>
-    <GoogleAuth />
+    <TodoList />
   </Provider>,
   rootElement
 );
